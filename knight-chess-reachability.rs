dd2ef0d26cc9f4ee55ed705d92a8a46ff4e63cf2
LoganMunoz16 <munozl1@spu.edu>
2023-05-10 20:14:48 -0700

    Refactor to a matrix of Squares, Graph too Large

--- knight-chess-reachability.rs
+++ knight-chess-reachability.rs
@@ -15,68 +15,57 @@ const chessBoardSquares: [[&str; 8] ;8] = [["a8", "b8", "c8", "d8", "e8", "f8", 
                                       ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
                                       ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]];
 
-fn isValidMove(x : i32, y : i32, xMove : i32, yMove: i32) -> bool {
-    if (x + xMove > 8) || (x + xMove < 0) || (y + yMove > 8) || (y + yMove < 0){
+const knightMoves: [(i32, i32); 8] = [(1, 2), (-1, 2), (2, 1), (-2, 1), (2, -1), (-2, -1), (1, -2), (-1, -2)];
+
+fn isValidMove(position : (i32, i32), knightMove : (i32, i32)) -> bool {
+    if (position.0 + knightMove.0 > 8) || (position.0 + knightMove.0 < 0) || (position.1 + knightMove.1 > 8) || (position.1 + knightMove.1 < 0){
         return false;
     } else {
         return true;
     }
 }
 
-struct Node<'a> {
-    name: &'a str,
-    moves: Option<Vec<Box<Node<'a>>>>,
-    path: Option<Vec<&'a str>>,
-    origin: Option<&'a str>
+struct Square {
+    label: (i32, i32),
+    path: Option<Vec<(i32, i32)>>,
+    origin: Option<(i32, i32)>
 }
 
-struct Graph<'a> {
-    nodes: Vec<Node<'a>>
+struct ChessBoard {
+    squares: Vec<Vec<Square<>>>
 }
 
-impl<'a> Graph<'a> {
+impl ChessBoard {
 
-    fn new() -> Graph<'a> {
-        let mut squaresList : Vec<Node<'a>> = Vec::new();
-        for row in chessBoardSquares {
-            for col in row {
-                let mut tempNode = Node {
-                    name: col,
-                    moves: None,
+    fn new() -> ChessBoard {
+        let mut squaresMatrix : Vec<Vec<Square<>>> = Vec::new();
+        let mut newBoard = ChessBoard {
+            squares: squaresMatrix,
+        };
+
+        for row in (0..8).rev() {
+            let mut thisRow : Vec<Square<>> = Vec::new();
+            for  col in 0..8 {
+                let mut tempSquare = Square {
+                    label: (row, col),
                     path: None,
                     origin: None
                 };
-                squaresList.push(tempNode);
+                thisRow.push(tempSquare);
             }
+            newBoard.squares.push(thisRow);
         }
-        let mut newGraph = Graph {
-            nodes: squaresList,
-        };
-        return newGraph;
-    }
-
-    fn addValidMoves(&self, takenPositions : Vec<&str>) {
-        for row in 0..8 {
-            for col in 0..8 {
-                let nodeIndex = col + row * 8;
-                for x in 0..3 {
-                    for y in 0..3 {
-                        //Iterate through all knight moves
-                        //Checking if valid
-                        //If valid, then add resulting node to moves list
-                        //Just use the offset values and formula above to find node in list
-                    }
-                }
-            }
-        }
+        return newBoard;
     }
 }
 
 
 fn main() {
-    let mut chessBoard = Graph::new();
-    for i in 0..chessBoard.nodes.len() {
-        println!("{}", chessBoard.nodes[i].name);
+    let mut chessBoard = ChessBoard::new();
+    for i in 0..8 {
+        for j in 0..8 {
+            println!("{}, {}", chessBoard.squares[i][j].label.0, chessBoard.squares[i][j].label.1);
+        }
     }
 }
 
